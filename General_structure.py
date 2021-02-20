644a2fddf0ce139a543f1f9522253fce2a8eaa9e
selwonkak <selwonkak@gmail.com>
2021-02-20 14:57:58 +0000

    General program structure added

--- General_structure.py
+++ General_structure.py
@@ -101,8 +101,89 @@ class explicit_matrices:
 
     # gates that act upon a single qubit with an operation (reminder to send the qubit number)
 
+
     # empty (for now, we want every single state in this form: compose them from a 1 qubit gate + identity and
     # repeated tensor products to combine if necessary. setup depends on qubit number and which qubit is acted on)
+
+    # gates that act on the list of qubits sent:
+
+    def hadamard_list(self, list):
+        '''applies the hadamard gate to each qubit in the list indices sent (zero based)'''
+        # approach: repeatedly tensor product either an identity or gate depending on if the item is in the list
+
+        init = np.ones([2, 2], dtype=complex)  # set up array
+        init[1][1] = -1  # correct the values to get a hadamard gate
+        I = np.array([[1, 0], [0, 1]])
+        if 0 in list:
+            H = init * (1 / np.sqrt(2))  # normalisation
+        else:
+            H = I
+        H_single = init * (1 / np.sqrt(2))
+
+        for val in range(self.qubit_n - 1):  # v slow, but works
+            if val+1 in list:
+                H = self.tensor_product(H, H_single)
+            else:
+                H = self.tensor_product(H, I)
+        # H now is the full matrix for a hadamard gate that will be applied to every qubit in an entangled state of
+        # self.n size
+
+        # applying the matrix to the current state of the system to
+        self.state = H.dot(self.state)
+
+    def x_list(self, list):
+        # approach: repeatedly tensor product either an identity or gate depending on if the item is in the list
+        X = np.ones([2, 2], dtype=complex)
+        X[0][0] = 0
+        X[1][1] = 0
+        X_single = X
+        I = np.array([[1, 0], [0, 1]])
+        if not 0 in list:
+            X = I # special case for if the first qubit does not have an x gate across it (zero isn't in list)
+        for val in range(self.qubit_n - 1):  # v slow, but works
+            if val+1 in list:
+                X = self.tensor_product(X, X_single)
+            else:
+                X = self.tensor_product(X, I)
+        self.state = X.dot(self.state)
+
+    def y_list(self, list):
+        # approach: repeatedly tensor product either an identity or gate depending on if the item is in the list
+        Y = np.zeros([2, 2], dtype=complex)
+        Y[0][1] = _i
+        Y[1][0] = i
+        Y_single = Y
+        I = np.array([[1, 0], [0, 1]])
+        if not 0 in list:
+            Y = I # special case for if the first qubit does not have an x gate across it (zero isn't in list)
+        for val in range(self.qubit_n - 1):  # v slow, but works
+            if val+1 in list:
+                Y = self.tensor_product(Y, Y_single)
+            else:
+                Y = self.tensor_product(Y, I)
+
+        self.state = Y.dot(self.state)
+
+    def z_list(self, list):
+        # approach: repeatedly tensor product either an identity or gate depending on if the item is in the list
+        Z = np.zeros([2, 2], dtype=complex)
+        Z[0][0] = 1
+        Z[1][1] = -1
+        Z_single = Z
+        I = np.array([[1, 0], [0, 1]])
+        if not 0 in list:
+            Z = I  # special case for if the first qubit does not have an x gate across it (zero isn't in list)
+        for val in range(self.qubit_n - 1):  # v slow, but works
+            if val + 1 in list:
+                Z = self.tensor_product(Z, Z_single)
+            else:
+                Z = self.tensor_product(Z, I)
+
+        self.state = Z.dot(self.state)
+
+
+
+
 
     # controlled gates (act on multiple qubits, in some more complicated way)
 
@@ -157,8 +238,9 @@ def main():
             self.qubit_n = len(bits)
             assert self.n == 2 ** self.qubit_n
             print("Initial state:")
+            print(bits)
+            print("entangled: ")
             print(self.state)
-            print(self.n)
             print("-----------------------")
 
         def entangle(self, bits):
@@ -194,15 +276,31 @@ def main():
 
     # example of how to use the class:
 
+    np.set_printoptions(formatter={'complex_kind': '{:.5f}'.format}) # formatting, hides floating point errors from
+    # making messy looking outputs
     # N qubit grovers (sifting for 0):
-    test = state([[1, 0], [1, 0], [1, 0]])
-    test.hadamard_all()
+    input = [[1, 0], [1, 0], [1, 0]]
+    test = state(input)
+    test.hadamard_list([0, 1, 2])
     test.c_z_last()
     test.z_all()
     test.c_z_last()
     test.hadamard_all()
     print(test.get_state())
+    print("")
 
+    # N qubit grovers (sifting for 1):
+    input = [[1, 0], [1, 0], [1, 0]]
+    test = state(input)
+    test.hadamard_all()
+    xlist = list(range(0, test.qubit_n -1))
+    test.x_list(xlist)
+    test.c_z_last()
+    test.x_list(xlist)
+    test.z_all()
+    test.c_z_last()
+    test.hadamard_all()
+    print(test.get_state())
     # right now, most of the gates are a huge mess so it all needs to be more consistent.
 
 
