1cfa66704f206e314358ddb6547092dfdfe8106e
rohan-nittur <rohan.nittur@gmail.com>
2021-02-20 22:44:15 +0000

    General grover

---
+++ Grover.py
@@ -0,0 +1,58 @@
+import numpy as np
+import gates
+
+def tensorProduct(U,V):
+    # Tensor product of 2 matrices
+    (rU,cU), (rV,cV) = U.shape, V.shape
+    result = np.zeros((rU*rV,cU*cV))
+    for r in range(rU):
+        for c in range(cU):
+            result[r*rV:(r+1)*rV,c*cV:(c+1)*cV] = U[r,c] * V
+    return result
+
+def tensorPower(U,n):
+    # Matrix U tensored with itself n times
+    result = U
+    for i in range(n-1):
+        result = tensorProduct(result,U)
+    return result
+
+def constructOracle(n,ws):
+    # Create n qubit dummy oracle with ws as solution states
+    oracle = np.eye(pow(2,n))
+    for w in ws:
+        oracle[w,w] = -1
+    return oracle
+
+def cnZ(n):
+    # Matrix for controlled ^ n Z gate
+    # There are n-1 control bits
+    cnZ = np.eye(pow(2,n))
+    cnZ[-1,-1] = -1
+    return cnZ
+
+def grover(n,ws):
+    # Initialise the register
+    qState = tensorPower(gates.ZERO,n)
+    hadamardAll = tensorPower(gates.H,n)
+    qState = np.dot(hadamardAll,qState)
+    # Perform rotation
+    # Change number in for loop - getting weird results
+    for iteration in range(1):
+        # Apply the phase oracle
+        oracle = constructOracle(n,ws)
+        qState = np.dot(oracle,qState)
+        # Probability amplification (diffuser)
+        qState = np.dot(hadamardAll,qState)
+        xAll = tensorPower(gates.X,n)
+        qState = np.dot(xAll,qState)
+        qState = np.dot(cnZ(n),qState)
+        qState = np.dot(xAll,qState)
+        qState = -np.dot(hadamardAll,qState)
+    # Operations introduce a global phase, hence the -
+    # This does not matter when it is measured
+    print("Final register state:")
+    print(qState)
+    gates.MeasureAll(qState)
+
+grover(3,[5,6])
--- gates.py
+++ gates.py
@@ -1,20 +1,5 @@
 import numpy as np
 
-def tensorProduct(U,V):
-    # Naïve tensor product implementation
-    (rU,cU), (rV,cV) = U.shape, V.shape
-    result = np.zeros((rU*rV,cU*cV))
-    for r in range(rU):
-        for c in range(cU):
-            result[r*rV:(r+1)*rV,c*cV:(c+1)*cV] = U[r,c] * V
-    return result
-
-def tensorPower(U,n):
-    result = U
-    for i in range(n-1):
-        result = tensorProduct(result,U)
-    return result
-
 ZERO = np.array([[1],[0]])
 ONE = np.array([[0],[1]])
 
@@ -26,6 +11,11 @@ H = np.array([
 X = np.array([
     [0,1],
     [1,0]
+])
+
+Z = np.array([
+    [1,0],
+    [0,-1]
 ])
 
 I = np.eye(2)
@@ -50,42 +40,3 @@ def MeasureAll(register):
     measurement = np.searchsorted(cum_prob,r)
     print(f"Collapsed into basis state |{measurement}> (|{bin(measurement)[2:]}>)")
     return measurement
-
-
-def entangle():
-    # Extremely simple circuit to entangle 2 qubits
-    qRegister = tensorProduct(ZERO,ZERO) # Start with a register of 2 qubits
-    operator1 = tensorProduct(H,H) # The first operator is an H gate on qubit 0
-    qRegister = np.dot(operator1,qRegister) # Apply it to the register
-    # print(qRegister)
-    qRegister = np.dot(cNOT,qRegister) # Apply a CNOT gate to the register
-    # print(qRegister)
-    MeasureAll(qRegister) # Measure the state of the register
-
-def grover2bit(q1,q2):
-    # 2 qubit Grover
-<<<<<<< HEAD
-    qRegister = tensorProduct(q1,q2)
-=======
-    qRegister = tensorProduct(ZERO,ZERO)
->>>>>>> f452cff63ed5b26e30908c223280d82cc7926114
-    HH = tensorProduct(H,H)
-    qRegister = np.dot(HH,qRegister)
-    qRegister = np.dot(cZ,qRegister)
-    qRegister = np.dot(HH,qRegister)
-    XX = tensorProduct(X,X)
-    qRegister = np.dot(XX,qRegister)
-    qRegister = np.dot(cZ,qRegister)
-    qRegister = np.dot(XX,qRegister)
-    qRegister = np.dot(HH,qRegister)
-    print(f"State of quantum register : \n {qRegister}\n")
-    print("Measuring...\n")
-    MeasureAll(qRegister)
-
-<<<<<<< HEAD
-# grover2bit(ZERO,ZERO)
-
-print(tensorProduct(X,I))
-=======
-grover2bit()
->>>>>>> f452cff63ed5b26e30908c223280d82cc7926114
