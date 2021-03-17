1e7e98731cea90f9b04e5153c30db0b8e9ff2c26
selwonkak <selwonkak@gmail.com>
2021-03-17 15:26:03 +0000

    Sparse matrices added, A mess right now

--- General_structure.py
+++ General_structure.py
@@ -207,10 +207,263 @@ class explicit_matrices:
 
     def oracle_general(self, ws):
         # Create n qubit dummy oracle with ws as solution states
+
         oracle = np.eye(pow(2, int(self.qubit_n)))
         for w in ws:
             oracle[w, w] = -1
-        return np.dot(oracle, self.state)
+        #print(oracle)
+        #print(self.state)
+        self.state = np.dot(oracle, self.state)
+
+
+from SparseMatrix import SparseMatrix
+
+class sparse_matrices:
+
+
+    def fromDense(self, M):
+        assert M.shape[0] == M.shape[1]
+        sp = SparseMatrix(M.shape[0])
+        for col in range(M.shape[0]):
+            for row in range(M.shape[0]):
+                if M[row, col] != 0:
+                    sp[row, col] = M[row, col]
+        return sp
+
+    '''ZERO = np.array([1, 0])
+    ONE = np.array([0, 1])
+
+    H = fromDense(np.array([
+        [1, 1],
+        [1, -1]
+    ]) / np.sqrt(2))
+
+    X = fromDense(np.array([
+        [0, 1],
+        [1, 0]
+    ]))
+
+    Z = fromDense(np.array([
+        [1, 0],
+        [0, -1]
+    ]))
+
+    I = fromDense(np.eye(2))
+
+    cX = fromDense(np.array([
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 0, 1],
+        [0, 0, 1, 0]
+    ]))
+
+    cZ = fromDense(np.array([
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, -1]
+    ]))
+'''
+    def cnZ(n):
+        # Matrix for controlled ^ n Z gate
+        # There are n-1 control bits
+        cnZ = np.eye(pow(2, n))
+        cnZ[-1, -1] = -1
+        return fromDense(cnZ)
+
+    def constructOracle(n, ws):
+        # Create n qubit dummy oracle with ws as solution states
+        oracle = np.eye(pow(2, n))
+        for w in ws:
+            oracle[w, w] = -1
+        return fromDense(oracle)
+
+    def MeasureAll(register):
+        cum_prob = np.cumsum(register ** 2)
+        r = np.random.rand()
+        measurement = np.searchsorted(cum_prob, r)
+        print(f"Collapsed into basis state |{measurement}> (|{bin(measurement)[2:]}>)")
+        return measurement
+
+    def hadamard_all(self):
+        '''applies the hadamard gate to each qubit in the entangled state (by reference)'''
+        n = 2  # reminder- needs generalising
+
+        H = self.fromDense(np.array([
+            [1, 1],
+            [1, -1]
+        ]) / np.sqrt(2))
+
+        H = H**self.qubit_n
+        print(H)
+        self.state = H @ self.state
+
+    def x_all(self):
+        X = self.fromDense(np.array([
+            [0, 1],
+            [1, 0]
+        ]))
+
+        X = X**self.qubit_n
+
+        self.state = X @ self.state
+
+    def y_all(self):
+        n = 2
+        Y = np.zeros([2, 2], dtype=complex)
+        Y[0][1] = _i
+        Y[1][0] = i
+        Y_single = Y
+        for val in range(self.qubit_n - 1):  # v slow, but works
+            Y = self.tensor_product(Y, Y_single)
+
+        self.state = Y.dot(self.state)
+
+    def z_all(self):
+        Z = self.fromDense(np.array([
+            [1, 0],
+            [0, -1]
+        ]))
+        Z = Z**self.qubit_n
+
+        self.state = Z @ self.state
+
+    def phase_all(self, phi):
+        # angle can be changed easily, set in pi-radians
+        n = 2
+        P = np.zeros([2, 2], dtype=complex)
+        P[0][0] = 1
+        P[1][1] = np.exp(i * phi * np.pi)
+
+        for val in range(self.qubit_n - 1):  # v slow, but works
+            P = self.tensor_product(P, P)
+
+        self.state = P.dot(self.state)
+
+    # gates that act upon a single qubit with an operation (reminder to send the qubit number)
+
+    # empty (for now, we want every single state in this form: compose them from a 1 qubit gate + identity and
+    # repeated tensor products to combine if necessary. setup depends on qubit number and which qubit is acted on)
+
+    # gates that act on the list of qubits sent:
+
+    def hadamard_list(self, list):
+        '''applies the hadamard gate to each qubit in the list indices sent (zero based)'''
+        # approach: repeatedly tensor product either an identity or gate depending on if the item is in the list
+
+        H_single = self.fromDense(np.array([
+            [1, 1],
+            [1, -1]
+        ]) / np.sqrt(2))
+        I = fromDense(np.eye(2))
+
+        init = np.ones([2, 2], dtype=complex)  # set up array
+        init[1][1] = -1  # correct the values to get a hadamard gate
+        if 0 in list:
+            H = H_single
+        else:
+            H = I
+
+        for val in range(self.qubit_n - 1):  # v slow, but works
+            if val + 1 in list:
+                H = H * H_single
+            else:
+                H = H * I
+        # H now is the full matrix for a hadamard gate that will be applied to every qubit in an entangled state of
+        # self.n size
+
+        # applying the matrix to the current state of the system to
+        self.state = H @ self.state
+
+    def x_list(self, list):
+        # approach: repeatedly tensor product either an identity or gate depending on if the item is in the list
+        X_single = self.fromDense(np.array([
+            [0, 1],
+            [1, 0]
+        ]))
+        I = fromDense(np.eye(2))
+        if not 0 in list:
+            X = I  # special case for if the first qubit does not have an x gate across it (zero isn't in list)
+        else:
+            X = X_single
+        for val in range(self.qubit_n - 1):  # v slow, but works
+            if val + 1 in list:
+                X = X*X
+            else:
+                X = X*I
+
+        self.state = X @ self.state
+
+    def y_list(self, list):
+        # approach: repeatedly tensor product either an identity or gate depending on if the item is in the list
+        Y = np.zeros([2, 2], dtype=complex)
+        Y[0][1] = _i
+        Y[1][0] = i
+        Y_single = Y
+        I = np.array([[1, 0], [0, 1]])
+        if not 0 in list:
+            Y = I  # special case for if the first qubit does not have an x gate across it (zero isn't in list)
+        for val in range(self.qubit_n - 1):  # v slow, but works
+            if val + 1 in list:
+                Y = self.tensor_product(Y, Y_single)
+            else:
+                Y = self.tensor_product(Y, I)
+
+        self.state = Y.dot(self.state)
+
+    def z_list(self, list):
+        # approach: repeatedly tensor product either an identity or gate depending on if the item is in the list
+        Z_single = self.fromDense(np.array([
+            [1, 0],
+            [0, -1]
+        ]))
+        I = fromDense(np.eye(2))
+        if not 0 in list:
+            Z = I  # special case for if the first qubit does not have an x gate across it (zero isn't in list)
+        else:
+            Z = Z_single
+        for val in range(self.qubit_n - 1):  # v slow, but works
+            if val + 1 in list:
+                Z = Z * Z
+            else:
+                Z = Z * I
+
+        self.state = Z @ self.state
+
+    # controlled gates (act on multiple qubits, in some more complicated way)
+
+    def c_z_last(self):
+        '''controlled z operating on the last qubit'''
+        C = np.zeros([self.n, self.n], dtype=complex)
+        for i in range(self.n):
+            C[i, i] = 1
+
+        C[self.n - 1, self.n - 1] = -1
+        # print("aaa")
+        # print(self.n)
+        # print(C)
+        self.state = C.dot(self.state)
+
+    def cnot(self):
+        n = 4
+        C = np.zeros([self.n, self.n])
+        C[0][0] = 1
+        C[1][1] = 1
+        C[2][3] = 1
+        C[3][2] = 1
+
+        self.state = C.dot(self.state)
+
+    def oracle_general(self, ws):
+        # Create n qubit dummy oracle with ws as solution states
+
+        oracle = np.eye(pow(2, int(self.qubit_n)))
+        for w in ws:
+            oracle[w, w] = -1
+        # print(oracle)
+        # print(self.state)
+        self.state = np.dot(oracle, self.state)
+
 
 
 class programs:
@@ -220,12 +473,17 @@ class programs:
 
     def grovers(state, ws):
         '''Runs grovers on a state object, with the given ws for the oracle'''
+
         state.hadamard_all()
         # Perform rotation
         # Change number in for loop - getting weird results
-        for iteration in range(4):
-            # Apply the phase oracle
 
+        # Perform rotation
+        # Change number in for loop - getting weird results
+        i = 4* int(state.qubit_n ** (1/2))  # iteration num = sqrt N * some factor
+        print(i)
+        for iteration in range(40):
+            # Apply the phase oracle
             state.oracle_general(ws)
             # Probability amplification (diffuser)
             state.hadamard_all()
@@ -234,12 +492,89 @@ class programs:
             state.hadamard_all()
         return state
 
+    def error_correction(state):
+        from QuantumRegister import QuantumRegister
+        import sparseGates as gates
+
+        # Initialise a 9 qubit quantum register
+        qState = QuantumRegister(9)
+        # Combine first 3 gates into one for efficiency
+        zhz = gates.Z @ gates.H @ gates.Z
+        # Apply tensor product to find the unitary operator
+        U = zhz * gates.I ** 8
+        # Apply it to the state
+
+
+
+        qState = U @ qState
+        qState = (gates.cX * gates.I ** 7) @ qState
+        doubleH = (gates.H * gates.H) * gates.I ** 7
+        qState = doubleH @ qState
+        cnot02 = gates.cXs(9, 0, 2)
+        qState = cnot02 @ qState
+        cnot12 = gates.cXs(9, 1, 2)
+        qState = cnot12 @ qState
+        H2 = gates.I ** 2 * gates.H * gates.I ** 6
+        qState = H2 @ qState
+        cnot03 = gates.cXs(9, 0, 3)
+        qState = cnot03 @ qState
+        cnot23 = gates.cXs(9, 2, 3)
+        qState = cnot23 @ qState
+        H04 = gates.H * gates.I ** 2 * gates.H * gates.I ** 5
+        qState = H04 @ qState
+        cnot04 = gates.cXs(9, 0, 4)
+        qState = cnot04 @ qState
+        cnot14 = gates.cXs(9, 1, 4)
+        qState = cnot14 @ qState
+        cnot24 = gates.cXs(9, 2, 4)
+        qState = cnot24 @ qState
+        Hs = gates.H ** 2 * gates.I ** 3 * gates.H ** 4
+        qState = Hs @ qState
+        errorGate = gates.I ** 3 * gates.Z * gates.I ** 5
+        qState = errorGate @ qState
+        cz54 = gates.cZs(9, 5, 4)
+        qState = cz54 @ qState
+        cnot53 = gates.cXs(9, 5, 3)
+        qState = cnot53 @ qState
+        cnot52 = gates.cXs(9, 5, 2)
+        qState = cnot52 @ qState
+        cz51 = gates.cZs(9, 5, 1)
+        qState = cz51 @ qState
+        cnot64 = gates.cXs(9, 6, 4)
+        qState = cnot64 @ qState
+        cnot63 = gates.cXs(9, 6, 3)
+        qState = cnot63 @ qState
+        cz62 = gates.cZs(9, 6, 2)
+        qState = cz62 @ qState
+        cz60 = gates.cZs(9, 6, 0)
+        qState = cz60 @ qState
+        cnot74 = gates.cXs(9, 7, 4)
+        qState = cnot74 @ qState
+        cz73 = gates.cZs(9, 7, 3)
+        qState = cz73 @ qState
+        cz71 = gates.cZs(9, 7, 1)
+        qState = cz71 @ qState
+        cnot70 = gates.cXs(9, 7, 0)
+        qState = cnot70 @ qState
+        cz84 = gates.cZs(9, 8, 4)
+        qState = cz84 @ qState
+        cz82 = gates.cZs(9, 8, 2)
+        qState = cz82 @ qState
+        cnot81 = gates.cXs(9, 8, 1)
+        qState = cnot81 @ qState
+        cnot80 = gates.cXs(9, 8, 0)
+        qState = cnot80 @ qState
+        finalHs = gates.I ** 5 * gates.H ** 4
+        qState = finalHs @ qState
+        qState.MeasureAll()
+
     def measure(state):
         '''returns a quantum measurement on the state object'''
         cum_prob = np.cumsum(state.get_state() ** 2)
         r = np.random.rand()
         measurement = np.searchsorted(cum_prob, r)
         print(f"Collapsed into basis state |{measurement}> (|{bin(measurement)[2:]}>)")
+        print(f"P(|{measurement}>) = {state.get_state()[measurement] ** 2}")
         return measurement
 
     def print_register(state):
@@ -257,8 +592,9 @@ def main():
 
     # user input goes here, to decide program, calculation method, inputs.
 
-    input = [[1, 0], [0, 1], [0, 1]]  ###### placeholder: will need to read in data and validate
+    #input = [[1, 0], [0, 1], [0, 1]]  ###### placeholder: will need to read in data and validate
     methods = explicit_matrices  # by changing this, you can change which class is used for the processing of gates.
+    methods = sparse_matrices
 
     # can be user defined from a list using a case statement possibly?
 
@@ -302,6 +638,9 @@ def main():
                 total += component ** 2
             return vector / (total ** (1 / 2))
 
+        def __matmul__(self, operator):
+            return operator @ self
+
         def get_state(self):
             return self.state
 
@@ -310,16 +649,16 @@ def main():
 
     # every time I write 'run on the state', I mean send the state object (not just tbe vector) to the method.
 
-    # all the outpus/ calculations should go here: create a state with the given inputs,
+    # all the outputs/ calculations should go here: create a state with the given inputs,
     # run the needed programs on that state
     # to output a measurement, simply run programs.measure on the state
 
     # example of how to use the class:
     print("starting")
     print("...")
-    input = [[1, 0], [0, 1], [0, 1]]
+    input = [[1, 0], [1, 0]]#, [1, 0], [1, 0], [1, 0]]
     my_state = state(input)
-    final_state = programs.grovers(my_state, [5, 6])
+    final_state = programs.grovers(my_state, [1])
     programs.print_register(final_state)
     programs.measure(final_state)
     print("")
