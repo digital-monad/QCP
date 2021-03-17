db1000a6bb1d12ea32494f0c60d50f76751192d5
selwonkak <selwonkak@gmail.com>
2021-03-17 23:46:35 +0000

    issues with grovers fixed

--- General_structure.py
+++ General_structure.py
@@ -481,6 +481,29 @@ class programs:
             state.hadamard_all()
         return state
 
+    def qft(n):
+        from QuantumRegister import QuantumRegister
+        import sparseGates as gates
+        state = QuantumRegister(4)
+        # Start with an n qubit register
+        for rot in range(n - 1):
+            hn = gates.H
+            hn = gates.I ** rot * gates.H if rot > 0 else hn
+            hn = hn * gates.I ** (n - rot - 1)
+            state = hn @ state
+            for qubit in range(n - 1):
+                rotation = gates.CROT(n, qubit + 1, rot, qubit + 2)
+                state = rotation @ state
+        # hadamard the last qubit
+        hn = gates.I ** (n - 1) * gates.H
+        state = hn @ state
+        # Swap pairs of qubits
+        for qubit in range(n // 2):
+            state = gates.swap(n, qubit, n - qubit - 1) @ state
+        print(np.sum(state ** 2))
+
+        state.MeasureAll()
+        
     def error_correction(state):
         from QuantumRegister import QuantumRegister
         import sparseGates as gates
@@ -673,7 +696,7 @@ def UI():
     menu = int(input("Menu options \n (1) Custom Grovers \n (2) 9 Quibit Grovers with error correction \n (3) Shor's n Qubit \n (4) exit \n : "))
     if menu == 4:
         quit()
-    input1 = int(input("Please provide choice of algorythm from options \n (1) Grover \n (2) Shor \n : "))
+    input1 = int(input("Please provide choice of algorithm from options \n (1) Grover \n (2) Shor \n : "))
     print()
 
     if menu == 1:
