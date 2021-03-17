7499ae256d4237fa8c76c57e84175d455739fd6c
selwonkak <selwonkak@gmail.com>
2021-03-17 23:53:49 +0000

    issues with grovers fixed

--- General_structure.py
+++ General_structure.py
@@ -480,6 +480,29 @@ class programs:
             state.hadamard_all()
         return state
 
+    def qft(n):
+        import numpy as np
+        from QuantumRegister import QuantumRegister
+        import sparseGates as gates
+
+        state = QuantumRegister(n)
+        for rot in range(n - 1):
+            hn = gates.H
+            hn = gates.I ** rot * gates.H if rot > 0 else hn
+            hn = hn * gates.I ** (n - rot - 1)
+            state = hn @ state
+            for qubit in range(n - 1):
+                rotation = gates.CROT(n, qubit + 1, rot, qubit + 2)
+                state = rotation @ state
+        # hadamard the last qubit
+        hn = gates.I ** (n - 1) * gates.H
+        state = hn @ state
+        # Swap pairs of qubits
+        for qubit in range(n // 2):
+            state = gates.swap(n, qubit, n - qubit - 1) @ state
+        print(np.sum(state ** 2))
+        state.MeasureAll()
+
     def error_correction(state):
         # does not follow main program structure due to needing more gates not yet implemented
         # runs quantum error correction
